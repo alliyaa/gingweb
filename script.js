3249b3c8087f4c7af1f7013564a84ca5e24e634f
alliyaa <alliyaahmad#@gmail.com>
2024-07-28 15:55:43 +0300

    fixed issue

--- script.js
+++ script.js
@@ -77,26 +77,27 @@ window.addEventListener('scroll', () => {
 });
 
 
-document.addEventListener('DOMContentLoaded', () => {
 
-    const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+    document.addEventListener('DOMContentLoaded', () => {
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+    
+        hamburger.addEventListener("click", mobileMenu);
+    
+        function mobileMenu() {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        }
+    
+        const navLink = document.querySelectorAll(".nav-link");
+    
+        navLink.forEach(n => n.addEventListener("click", closeMenu));
+    
+        function closeMenu() {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        }
 
     document.getElementById('themeToggle').addEventListener('click', toggleTheme);
     document.getElementById('searchBar').addEventListener('keyup', filterCountries);
